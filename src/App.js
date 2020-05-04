707d6cb6232f7b36200a1c73d0909955097cea72
ahadsenja <hamdanilm96@gmail.com>
2020-05-04 08:10:34 +0800

    chore: adding browser router for landing page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "assets/scss/style.scss";
+import LansingPage from "pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Route path="/" component={LansingPage} />
+      </Router>
     </div>
   );
 }
