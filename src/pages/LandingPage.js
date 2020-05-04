bfe6b0753d593127ae315e7811182425abfaabd4
ahadsenja <hamdanilm96@gmail.com>
2020-05-04 08:08:56 +0800

    feat: import landing page header component

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from "parts/Header";
+
+export default class LansingPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
